489f5d9f490a62e9a8218deb3f53ab110dc4bd76
Nindwen <nindweniel@gmail.com>
2016-02-20 01:36:11 +0200

    cont

--- slurp.sh
+++ slurp.sh
@@ -1,23 +1,33 @@
 #!/bin/bash
 
+style() {
+      sass --style compressed --update sass:output/css
+}
+
 watch() {
       sass --style compressed --watch sass/:output/css
 }
 
 page() {
-      markdown $1.md | cat head.html - foot.html | tidy -i -utf8 > output/$1.html
+      markdown $1.md | cat head.html - foot.html | tidy -iq -utf8 > output/$1.html
 }
 
 help() {
       echo "apuva!"
 }
 
+build() {
+      style
+      page index
+}
+
 push() {
+      build
       rsync -a output/ usvs:www/nindwen.blue
 }
 
 if [ -z "$1" ]; then 
-      echo usage: $0 [sass, watch]
+      push
       exit
 fi
 
